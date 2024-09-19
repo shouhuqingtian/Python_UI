297afe2edcbf8549301eb4b2422e72c58e4c0dbe
liu51 <519328539@qq.com>
2024-09-19 18:08:56 +0800

    Changes

---
+++ test01.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/9/14
+# @Author  : Bin
+# @Software: Aqua
+# @File    : test01.py
+
+
+# a = 1
+# b = 2
+# c = a+b
+# print(c)
+"""
+from faker import Faker
+
+fake = Faker('zh_CN')
+
+fake.name()
+
+for _ in range(100):
+    print(fake.name(), fake.address(), fake.email(), fake.user_name(), fake.bank(), fake.language_name(), fake.city_name(),
+    fake.android_platform_token(),end ='\t', sep=',')
+"""
+
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://192.168.16.50:10006/login?redirect=%2FleaseManagement%2Flease%3Ftype%3Dnav')
