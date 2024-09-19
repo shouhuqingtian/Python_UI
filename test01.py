3ef1c895e407563e469260ad7870bc199d9dc641
liu51 <519328539@qq.com>
2024-09-19 18:25:49 +0800

    第一次提交

--- test01.py
+++ test01.py
@@ -20,4 +20,4 @@ for _ in range(100):
     print(fake.name(), fake.address(), fake.email(), fake.user_name(), fake.bank(), fake.language_name(), fake.city_name(),
     fake.android_platform_token(),end ='\t', sep=',')
 """
-1
+
