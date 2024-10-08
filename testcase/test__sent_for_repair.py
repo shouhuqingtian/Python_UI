2d1997ad0fc262b1c23a0dbc2c060d7033c8230d
liu51 <519328539@qq.com>
2024-10-08 18:32:21 +0800

    完成送修测试用例书写

--- testcase/test__sent_for_repair.py
+++ testcase/test__sent_for_repair.py
@@ -3,11 +3,13 @@
 # @Author  : Bin
 # @Software: Aqua
 # @File    : test__sent_for_repair.py
+from sys import implementation
 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from time import sleep
+
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -16,26 +18,34 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 def test_parts_repair(driver):
-    driver.find_element(By.XPATH, "//span[contains(text(),'维修管理')]").click()
+    driver.implicitly_wait(10)
+    driver.find_element(By.XPATH, "(//span[contains(text(),'维修管理')])[1]").click()
     driver.find_element(By.XPATH, "//li/span[text()='送修审批']").click()
     driver.find_element(By.CLASS_NAME, "el-icon-plus").click()
     driver.find_element(By.XPATH, "(//input[@placeholder='请选择'])[2]").click()
-    sleep(10)
-    # # 使用 JavaScriptExecutor 来执行你的 JavaScript 代码
-    # js_code = """
-    # var element = document.evaluate("(//span[contains(text(),'零部件修理')])[4]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    # element.click();
-    # """
-    #
-    # # 执行 JavaScript 代码并点击元素
-    # driver.execute_script(js_code)
-    # driver.find_element(By.CSS_SELECTOR, ".el-button--primary:nth-child(4)").click()
-    driver.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='单元体修理'])[6]/following::li[1]").click()
-
-
-    # driver.find_element(By.XPATH, "(//span[contains(text(),'零部件修理')])[4]").click
-    driver.find_element(By.XPATH, "(//span[contains(text(),'选择')])[3]").click()
-
     sleep(2)
+
+    # 执行 JavaScript 代码并点击元素
+    repair_option = driver.find_element(By.XPATH, "//div[@x-placement='bottom-start']//span[contains(text(),'零部件修理')]")
+    driver.execute_script("arguments[0].click();", repair_option)
+    # 选择库存ID
+    driver.find_element(By.XPATH, "(//span[contains(text(),'选择')])[3]").click()
+    driver.find_element(By.XPATH, "(//div)[1862]").click()
+    driver.find_element(By.XPATH, "(//span[contains(text(),'确 定')])[12]").click()
+    # 选择 Vendor
+    driver.find_element(By.XPATH, "(//span[contains(text(),'选择')])[3]").click()
+    driver.find_element(By.XPATH, "(//div[contains(text(),'--')])[214]").click()
+    # 选择 Repair Party
+    driver.find_element(By.XPATH, "(//input[contains(@placeholder,'请选择')])[3]").click()
+    driver.find_element(By.XPATH, "(//li)[730]").click()
+    # QTY输入1
+    driver.find_element(By.XPATH, "(//input[contains(@type,'text')])[14]").sendkey("1")
+    driver.find_element(By.XPATH, "(//input[contains(@type,'number')])[2]").sendkey("1")
+    # 保存并申请审批
+    driver.find_element(By.XPATH, "(//span[contains(text(),'保存并申请审批')])[1]").click()
+
+
+
+sleep(2)
 
 
